2b01fafab176724e41a4a16babdbee39dd0e9473
Mariane Ribeiro <mari.ribeiro1994@gmail.com>
2025-05-03 15:12:45 -0300

    ajustes layout
    -adicionado testemunhas (parcial);
    - ajustado tema claro e tema escuro

--- js/utilities.js
+++ js/utilities.js
@@ -1,13 +1,17 @@
  // Ao carregar a página, aplica o tema salvo
  document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme") || "light";
+    const themeIcon = document.getElementById("theme-icon");
     document.body.setAttribute("data-theme", savedTheme);
+    themeIcon.className = savedTheme === "dark" ? "bi bi-sun" : "bi bi-moon";
   });
 
   // Alterna o tema e salva no localStorage
   function toggleTheme() {
     const currentTheme = document.body.getAttribute("data-theme");
+    const themeIcon = document.getElementById("theme-icon");
     const newTheme = currentTheme === "light" ? "dark" : "light";
     document.body.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
+    themeIcon.className = newTheme === "dark" ? "bi bi-sun" : "bi bi-moon";
   }
